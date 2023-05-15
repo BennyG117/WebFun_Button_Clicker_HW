a42846e36ff768a87f41334ea4b7022cbd22c258
BennyG117 <bengomezp@gmail.com>
2023-05-15 15:26:14 -0700

    Button Clicker Update

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 
+/* login-button-2 function to switch Login to: "Logout" */
+var loginButton = document.querySelector(".login-button-2");
+
+function  logSwitch() {
+    if(loginButton.innerText == "Logout") {
+        loginButton.innerText = "Login"
+    }
+else {
+    loginButton.innerText = "Logout"
+}    
+}
+
+
+
+
+/* add-def-button-8 emoves Add Definition button when clicked */
 function removeMe(element) {
     element.remove();
 }
+
+
